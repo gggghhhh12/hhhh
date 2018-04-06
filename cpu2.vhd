f83191d2b4cd8431221bc7edea13b9ae714885e4
gggghhhh12 <1196942855@qq.com>
2018-04-06 17:51:51 +0800

    hh

---
+++ cpu2.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:52:27 06/12/2017 
+-- Design Name: 
+-- Module Name:    cpu2 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity cpu2 is
+end cpu2;
+
+architecture Behavioral of cpu2 is
+
+begin
+
+
+end Behavioral;
+
